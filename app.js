9d30054a093f4420bf97936c9620613a22b94205
Asad-Hanif1 <hanifasad617@gmail.com>
2024-02-29 20:39:01 +0500

    index file creadted

---
+++ app.js
@@ -0,0 +1,115 @@
+// alert("hellow")
+// alert("jawad \ndream")
+
+// 4 tarha ki data types hoti ha
+
+// 1 string "ejdsn4383029"
+// 2 number 6738
+// 3Boolean true ya false
+// 4 ko badma dakhanga
+var userName = ("Asad Hanif")
+var age = (24)
+var education = ("S.E")
+var address = ("Malir City ")
+console.log(userName);
+console.log(age);
+console.log(education);
+console.log(address);
+console.log(userName + " " + age + " " + education + " " + address)
+var Age1 = 62;
+var Age2 = 63;
+var Age3 = "64";
+var Age4 = "65";
+console.log(Age2 + Age1);
+console.log(Age2 + " " + Age1);
+console.log(Age3 + " " + Age4);
+console.log(Age3 + + Age4);
+// var Age1 = prompt("enter first number");
+// var Age2 = prompt("enter second number");
+// var Age3 = +prompt("enter THIRD number");
+// var Age4 = +prompt("enter fourth number");
+console.log(Age1 + Age2);
+console.log(Age3 + Age4);
+var num1 = 20;
+// num1=num1+10
+// line number 34 ka 1 short form bhi ha wo nicha likhi ha
+num1 += 10
+// num1-=10
+// num1*=10
+// num1/=10
+console.log(num1 + 15);
+console.log(10 < 5);
+console.log(10 >= 10);
+console.log(10 <= 10);
+console.log(10 >= "10");
+console.log(9 <= "10");
+// xclination mark (!) ka madlab ha not
+console.log(10 != 10);
+console.log(10 == "10");
+console.log(10 !== "10");
+console.log(10 !== 10);
+// double equal to data type bhi check krta ha (==) LockManager(!) ka sath
+// && = and
+// || = OR
+var comparisions = 10 <= 10 || 10 < 6;
+console.log(comparisions)
+// (|| = OR)jitni bhi conditions ho unma sa agar 1 bhi true hui to wo nicha true batada ga answer ma
+// (& = and) is parallel of OR
+var comparision = 10 >= 10 && 10 > 6;
+console.log(comparision);
+
+
+// hum prom ma or console ma bracket () ka ander koi chez inverted commas "" ma jabhi likhta ha jub hum na wo chez first time likhi ho or baghir "" ka tub likhanga jub wo chez uper humna console ma ya var am likhi hui ho uper
+
+// var english = +prompt("english marks");
+// var math = +prompt ("math marks");
+// var obtain = +(english + math);
+// var totalmarks = 200;
+// console.log(obtain);
+// console.log(totalmarks);
+// console.log(obtain/totalmarks*100+"%")
+
+
+
+
+// var english = +prompt("english marks");
+// var math = +prompt("math marks");
+// var obtain= english + math;
+// var totalmarks= 200;
+// console.log(obtain);
+// console.log(totalmarks);
+// console.log(obtain/totalmarks*100 + "%");
+
+
+
+
+
+
+
+// if ka ander jo condition hogi wo agar true hui to if run kraga warna elserun kraga
+
+// var persentage = +prompt("what is your persentage")
+// if (persentage >= 50){
+//     console.log("malik pass hogia")
+// }
+// else{
+//     console.log("malik fail hogia")
+// }
+
+
+
+// var num = +prompt("number dal da")
+// if(num > 5 ){
+//     console.log("ifi") 
+// }
+// else{
+//     console.log('elses')
+// }
+
+
+
+
+
+
+
+
