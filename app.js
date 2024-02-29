73e94b54ebfb29c1b421440e5009ada51f48411f
Asad-Hanif1 <hanifasad617@gmail.com>
2024-02-29 21:52:34 +0500

    index file created

--- app.js
+++ app.js
@@ -7,56 +7,78 @@
 // 2 number 6738
 // 3Boolean true ya false
 // 4 ko badma dakhanga
-var userName = ("Asad Hanif")
-var age = (24)
-var education = ("S.E")
-var address = ("Malir City ")
-console.log(userName);
-console.log(age);
-console.log(education);
-console.log(address);
-console.log(userName + " " + age + " " + education + " " + address)
-var Age1 = 62;
-var Age2 = 63;
-var Age3 = "64";
-var Age4 = "65";
-console.log(Age2 + Age1);
-console.log(Age2 + " " + Age1);
-console.log(Age3 + " " + Age4);
-console.log(Age3 + + Age4);
+// var userName = ("Asad Hanif")
+// var age = (24)
+// var education = ("S.E")
+// var address = ("Malir City ")
+// console.log(userName);
+// console.log(age);
+// console.log(education);
+// console.log(address);
+// console.log(userName + " " + age + " " + education + " " + address)
+// var Age1 = 62;
+// var Age2 = 63;
+// var Age3 = "64";
+// var Age4 = "65";
+// console.log(Age2 + Age1);
+// console.log(Age2 + " " + Age1);
+// console.log(Age3 + " " + Age4);
+// console.log(Age3 + + Age4);
 // var Age1 = prompt("enter first number");
 // var Age2 = prompt("enter second number");
 // var Age3 = +prompt("enter THIRD number");
 // var Age4 = +prompt("enter fourth number");
-console.log(Age1 + Age2);
-console.log(Age3 + Age4);
-var num1 = 20;
+
+
+// console.log(Age1 + Age2);
+// console.log(Age3 + Age4);
+// var num1 = 20;
 // num1=num1+10
 // line number 34 ka 1 short form bhi ha wo nicha likhi ha
-num1 += 10
+
+
+
+
+
+// num1 += 10
 // num1-=10
 // num1*=10
 // num1/=10
-console.log(num1 + 15);
-console.log(10 < 5);
-console.log(10 >= 10);
-console.log(10 <= 10);
-console.log(10 >= "10");
-console.log(9 <= "10");
-// xclination mark (!) ka madlab ha not
-console.log(10 != 10);
-console.log(10 == "10");
-console.log(10 !== "10");
-console.log(10 !== 10);
+// console.log(num1 + 15);
+// console.log(10 < 5);
+// console.log(10 >= 10);
+// console.log(10 <= 10);
+// console.log(10 >= "10");
+// console.log(9 <= "10");
+// // xclination mark (!) ka madlab ha not
+// console.log(10 != 10);
+// console.log(10 == "10");
+// console.log(10 !== "10");
+// console.log(10 !== 10);
+
+
+
+
 // double equal to data type bhi check krta ha (==) LockManager(!) ka sath
 // && = and
 // || = OR
-var comparisions = 10 <= 10 || 10 < 6;
-console.log(comparisions)
+
+
+
+// var comparisions = 10 <= 10 || 10 < 6;
+// console.log(comparisions)
+
+
+
+
 // (|| = OR)jitni bhi conditions ho unma sa agar 1 bhi true hui to wo nicha true batada ga answer ma
 // (& = and) is parallel of OR
-var comparision = 10 >= 10 && 10 > 6;
-console.log(comparision);
+
+
+
+
+// var comparision = 10 >= 10 && 10 > 6;
+// console.log(comparision);
 
 
 // hum prom ma or console ma bracket () ka ander koi chez inverted commas "" ma jabhi likhta ha jub hum na wo chez first time likhi ho or baghir "" ka tub likhanga jub wo chez uper humna console ma ya var am likhi hui ho uper
@@ -108,7 +130,95 @@ console.log(comparision);
 
 
 
+// var persentage = +prompt("what is your persentage")
+// if (persentage >= 50){
+//     console.log("malik pass hogia")
+// }
+// else{
+//     console.log("malik fail hogia")
+// }
 
+
+
+
+
+
+
+var num = +prompt("hello bhai log")
+if(num > 5 ){
+    console.log("asad")
+}
+else{
+    console.log('here')
+}
+
+
+
+
+
+
+// var height = +prompt("Put the height")
+// if(height < 20){
+//     console.log("not accept")
+// }
+// else if( height > 30 ){
+//   console.log("Accepted")
+// }
+// else{
+//     console.log("just accepted")
+// }
+
+
+
+
+
+
+
+// var salery = +prompt("Please tell me a salery")
+// if(salery >= 100000){
+//     console.log("I will convince my father")
+// }
+// else if(salery<=80000){
+//    console.log("my father will never agree")
+// }
+// else{
+//     console.log("maybe my father will accept")
+// }
+
+
+
+
+
+
+// var Age = +prompt("Enter your age" )
+// if( Age >= 28 ){
+//     console.log(" abba nahi manga")
+// }
+// else if( 22 <= Age  ){
+//    console.log("ma abba ko manalongi ")
+// }
+// else if( 18 <= Age  ){
+//     console.log("bs thora bara hojaw ")
+//  }
+// else{
+//     console.log("bara ho ka a ")
+// }
+
+
+
+
+
+
+
+
+// var num = +prompt("code dala")
+// if( num > 100 ){
+//     console.log("code sahi ha")
+// }
+
+// else{
+//     console.log("code ghalat ha")
+// }
 
 
 
